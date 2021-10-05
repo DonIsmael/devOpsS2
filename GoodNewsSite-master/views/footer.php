9d633de2ac7a440c13db74f41bca7add67bf22f4
32494 <amine.nemmaoui@student.vinci.be>
2021-10-05 19:21:38 +0200

    ok

--- GoodNewsSite-master/views/footer.php
+++ GoodNewsSite-master/views/footer.php
@@ -1,5 +1,4 @@
 		<div id="pied">
-            Bonjour
 			<strong>Excellente journée qu'aujourd'hui le <?php echo $date ?></strong> :: 
 			<?php $time_end = microtime(true);
 				  $time = number_format(($time_end - $time_start)*1000,6);
